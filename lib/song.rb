3d6123edfd8572ccde896dd0b79e1c975cef24a5
David Little <littledavid175@gmail.com>
2019-07-31 23:10:11 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+class Song 
+  attr_accessor :artist, :name, :genre
+  
+  @@all = []
+  
+  def initialize(name)
+    @@all << self
+    @name = name
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def artist_name
+   if  @artist 
+     @artist.name
+   else 
+     nil 
+   end
+  end
+  
+  
+  
+  
+  
+end
